a7a64ef63d69b1c7e6620f1f10610b0f34287f54
Eunbi Lee <lyb6336@naver.com>
2024-06-19 00:07:34 +0900

    [Part3] 멤버십 등록 컨트롤러 테스트 추가

--- membership-service/src/test/java/com/fastcampuspay/membership/RegisterMembershipControllerTest.java
+++ membership-service/src/test/java/com/fastcampuspay/membership/RegisterMembershipControllerTest.java
@@ -1,10 +1,19 @@
 package com.fastcampuspay.membership;
 
 
+import com.fastcampuspay.membership.adapter.in.web.RegisterMembershipRequest;
+import com.fastcampuspay.membership.domain.Membership;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 /*
@@ -20,4 +29,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 (MockMvc는 스프링 MVC 애플리케이션의 테스트를 작성할 때 사용되는 프레임워크)
  */
 public class RegisterMembershipControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    public void testRegisterMembership() throws Exception {
+
+        RegisterMembershipRequest request = new RegisterMembershipRequest("name", "email", "address", false);
+
+        Membership membership = Membership.generateMember(
+                new Membership.MembershipId("1"),
+                new Membership.MembershipName("name"),
+                new Membership.MembershipEmail("email"),
+                new Membership.MembershipAddress("address"),
+                new Membership.MembershipIsValid(true),
+                new Membership.MembershipIsCorp(false)
+        );
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/membership/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(mapper.writeValueAsString(membership)));
+    }
 }
+//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

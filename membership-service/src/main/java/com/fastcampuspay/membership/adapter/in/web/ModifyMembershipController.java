9897d4a151dee45c51e127111772ed3051160bf3
leb <lyb6336@naver.com>
2024-06-19 18:13:02 +0900

    [Part3] 멤버십 정보 변경 추가 중

--- membership-service/src/main/java/com/fastcampuspay/membership/adapter/in/web/ModifyMembershipController.java
+++ membership-service/src/main/java/com/fastcampuspay/membership/adapter/in/web/ModifyMembershipController.java
@@ -18,14 +18,13 @@ public class ModifyMembershipController {
     private final ModifyMembershipUseCase modifyMembershipUseCase;
     @PostMapping(path = "/membership/modify/{membershipId}")
     ResponseEntity<Membership> modifyMembershipByMemberId(@RequestBody ModifyMembershipRequest request) {
-        //@PathVariable 어노테이션을 사용하여 {membershipId} 변수로 표시되는 URI의 템플릿 부분을 추출합니다 .
 
         /* request ~~
         request -> Command
         Usecase ~~(request를 받는게 아니라, command를 받아야함.)
         이렇게 하는 이유 : request 값이 바뀌더라도, 추가적으로 값을 더 추가하더라도. command라는 중간 계층을 이용해서 쉽게 호환성을 맞출 수 있음. 그래서 command를 이용
          */
-        FindMembershipCommand command = FindMembershipCommand.builder()
+        ModifyMembershipCommand command = ModifyMembershipCommand.builder()
                 .membershipId(membershipId)
                 .build();
 
